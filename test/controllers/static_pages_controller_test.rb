6363c4c44b3450b0f46cd0757f53f7c89f55b718
Eyal Liebermann <eyal.liebermann@fiverr.com>
2017-10-08 08:40:07 +0000

    add title manually on each page and suspend layout

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,16 +4,19 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | RoR Tutorial Sample App"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help | RoR Tutorial Sample App"
   end
   
   test "should get about controller response" do
     get static_pages_about_url
     assert_response :success
+    assert_select "title", "About | RoR Tutorial Sample App"
   end
 
 end
