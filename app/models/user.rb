aedbd3edb20ef227a8dba00069939594ba593aae
Eyal Liebermann <eyal.liebermann@fiverr.com>
2017-10-15 18:25:15 +0300

    add pasword to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX =  /\A[a-z]+[\w\-\.]*@([a-z]+\.)*[a-z]{2,}\z/i
   validates :email, presence:true, length: {maximum:255}, format: VALID_EMAIL_REGEX, uniqueness: {case_sensitive: false}
 
+  has_secure_password
 end
