f06aa60710eb86f784fcadd99c4f717cf5bf0810
Eyal Liebermann <eyal.liebermann@fiverr.com>
2017-10-15 18:12:32 +0300

    using the banf version of downcase

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  before_save { self.email = email.downcase }
+  before_save { email.downcase! }
 
   validates :name, presence:true, length: {maximum:50}
   VALID_EMAIL_REGEX =  /\A[a-z]+[\w\-\.]*@([a-z]+\.)*[a-z]{2,}\z/i
