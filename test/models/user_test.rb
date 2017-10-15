9be102806b52934330d50aec80124bcaa5d106d3
Eyal Liebermann <eyal.liebermann@fiverr.com>
2017-10-15 16:25:43 +0300

    add length vlidatios

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -23,6 +23,17 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
+  test "name should not exceed 50 characters" do
+    @user.name = 'a'*51
+    assert_not @user.valid?
+  end
+
+  test "email should not exceed 255 characters" do
+    at='@example.com'
+    @user.email = "a" * (255-at.length+1) +at
+    assert_not @user.valid?
+  end
+
   test "verify tests are independent" do
     assert @user.valid?
   end
