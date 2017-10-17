1b3872c93d55f3b4f2f36d337b573da7998fd11f
Eyal Liebermann <eyal.liebermann@fiverr.com>
2017-10-17 14:40:01 +0300

    add an optional size parameter

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
-  def gravatar_for (user)
+  def gravatar_for (user, size:80)
     digest= Digest::MD5::hexdigest user.email.downcase
-    gravatar_url = "https://secure.gravatar.com/avatar/#{digest}"
-    image_tag gravatar_url, alt:user.name, class: 'gravatar'
+    gravatar_url = "https://secure.gravatar.com/avatar/#{digest}?size=#{size}"
+    image_tag gravatar_url, alt:user.name, class: 'gravatar', size:size
   end
 end
