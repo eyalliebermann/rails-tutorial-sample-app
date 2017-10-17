d04d95b2649763ca126b40251b24ff3e14bba940
Eyal Liebermann <eyal.liebermann@fiverr.com>
2017-10-17 14:16:23 +0300

    implment gravatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+  def gravatar_for (user)
+    digest= Digest::MD5::hexdigest user.email.downcase
+    gravatar_url = "https://secure.gravatar.com/avatar/#{digest}"
+    image_tag gravatar_url, alt:user.name, class: 'gravatar'
+  end
 end
