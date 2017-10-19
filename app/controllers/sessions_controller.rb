6294c2f464855dfdc94314aa08989a399fa47e30
Eyal Liebermann <eyal.liebermann@fiverr.com>
2017-10-19 15:52:03 +0300

    solve flash bug by now

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       flash[:success] = "Hey #{user.name}.  You're logged in"
       render 'new'
     else
-      flash[:danger] = 'Oops. wrong email or password.'
+      flash.now[:danger] = 'Oops. wrong email or password.'
       render 'new'
     end
   end
